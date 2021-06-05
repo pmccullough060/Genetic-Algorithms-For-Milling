e0243625a2b6209883e910b22ae9ca67083ee271
pmccullough060 <pmccullough060@gmail.com>
2021-06-05 15:54:27 +0100

    finishing out equations... now to add tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-//Test stuff for getting the unit tests up and going.
 hello();
 
 export function hello(){
@@ -11,8 +9,18 @@ const feedPerTooth = 0.04;
 const toolDia = 10;
 const toolLength = 30;
 const noCuttingEdges = 4;
-const spindleSpeed = 500;
+const spindleSpeed = 1000;
 
+//these params will be set by the user.....
+const cutLength = 50;
+const cutAxial = 20;
+const cutRadial = 20;
+
+//Max tangential cutting force
+const tangForceMax = 500;
+
+//Specific cutting force alu
+const spefCuttingForce = 1000;
 
 class Member {
 
@@ -20,49 +28,85 @@ class Member {
       this.radial = radial;
       this.axial = axial;
       this.feedPerTooth = feedPerTooth;
+
+      this.compensatedFeedPerTooth();
    }
 
-   //Increasing the feed per tooth to compensate for radial chip thinning
    compensatedFeedPerTooth(){
+      //Increasing the feed per tooth to compensate for radial chip thinning
+      // radial = (mm), toolDia = (mm), feedPerTooth = (mm) 
       if(this.radial < toolDia/2){
-         this.feedPerTooth = (feedPerTooth * toolDia)/(2 * Math.sqrt(toolDia*this.axial - Math.pow(this.axial,2)));
+         this.feedPerTooth = (feedPerTooth * toolDia)/(2 * Math.sqrt(toolDia * this.radial - Math.pow(this.radial , 2)));
       }
    }
 
    cuttingSpeed(){
+      //spindleSpeed = (r/min), toolDia = (mm), cuttingSpeed = (m/min)
       return (spindleSpeed * toolDia * noCuttingEdges) / 1000;
    }
 
    tableFeed(){
-      return this.feedPerTooth * toolDia * spindleSpeed;
+      //feedperTooth = (mm), toolDia = (mm), spindleSpeed (r/min), tableFeed (mm/min)
+      return this.feedPerTooth * spindleSpeed * noCuttingEdges;
    }
 
    noPassesAxial(){
-      return 1;
+      //cutAxial = (mm)
+      return Math.ceil(cutAxial/this.axial);
    }
 
    noPassesRadial(){
-      return 1;
+      //cutRadial = (mm)
+      return Math.ceil(cutRadial/this.radial);
    }
 
    poorToolPositionFactor(){
-      if(this.axial >= toolDia/2){
-         return (this.axial / toolDia);
+      //Ranges from 0 (optimal) to 1 (poor)
+      //Accounts for "Over cutting" in the axial direction where axial > toolDia/2 which overheats the tool.
+      //The effect of radial chip thinning is mitigated by increasing the feedPerTooth so "light" cuts are not penalised.
+      //axial = (mm), toolDia = (mm)
+      if(this.axial > toolDia/2){
+         return ((this.axial-toolDia/2) / (toolDia/2));
       }
    }
 
-   fitness(){
-   
-      this.compensatedFeedPerTooth();
-      
-      var cuttingSpeed = cuttingSpeed();
-      var tableFeed = tableFeed();
+   totalCuttingTime(){
+      //table feed = (mm/min), cutLength (mm), result = (min)
+      return (this.noPassesAxial() * this.noPassesRadial() * cutLength) / this.tableFeed();
+   }
 
-      return -1;
+   netPower(){
+      //radial = (mm), axial = (mm), tableFeed = (mm/min), spefCuttingForce = (N/mm2), result = (kW)
+      return (this.radial * this.axial * this.tableFeed() * spefCuttingForce) / 60e6;
+   }
+
+   torque(){
+      //netPower = (kW), spindleSpeed (r/min), result = (Nm)
+      return (this.netPower() * 30e3) /(Math.PI * spindleSpeed);
+   }
+
+   tangentialForce(){
+      //torque = (Nm), toolDia (mm), result (N)
+      return this.torque() * toolDia / 1000;
+   }
+
+   fitness(){
+
+      //Tool will fail so particular member is not valid.
+      if(this.tangentialForce() >= this.tangForceMax){
+         return -1;
+      }
+
+      const cuttingTime = this.totalCuttingTime();
+
+      //The penalty for poor tool position can be up to 20% of the cutting time where radial = toolDia;
+      return cuttingTime + this.poorToolPositionFactor() * cuttingTime * 0.2;
    }
 }
 
-var mem = new Member(10,10, machiningParameters);
+var mem = new Member(5,10);
 mem.fitness();
+mem.torque();
 
+console.log(mem.torque());
 
